1a21b81a3f4cfe9eca08fce660b1aced526df66c
Baxter Ellard <baxter.ellard@gmail.com>
2018-01-31 16:23:16 -0500

    Copied all code into new project

--- Dragon 2/src/org/usfirst/frc/team6201/robot/Robot.java
+++ Dragon 2/src/org/usfirst/frc/team6201/robot/Robot.java
@@ -7,82 +7,77 @@
 
 package org.usfirst.frc.team6201.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import org.usfirst.frc.team6201.robot.subsystems.DriveTrain;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.usfirst.frc.team6201.robot.commands.ExampleCommand;
-import org.usfirst.frc.team6201.robot.subsystems.ExampleSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the TimedRobot
+ * functions corresponding to each mode, as described in the IterativeRobot
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the build.properties file in the
  * project.
  */
-public class Robot extends TimedRobot {
-	public static final ExampleSubsystem kExampleSubsystem
-			= new ExampleSubsystem();
-	public static OI m_oi;
-
-	Command m_autonomousCommand;
-	SendableChooser<Command> m_chooser = new SendableChooser<>();
-
+public class Robot extends IterativeRobot {
+	
+	public static final DriveTrain dt = new DriveTrain();
+	
+	public static OI oi;
+	
+	/**
+	 * 1 = rear left
+	 * 2 = front right
+	 * 3 = front left
+	 * 4 = rear right
+	 */
+/*
+	WPI_TalonSRX frontLeft = new WPI_TalonSRX(1);
+	WPI_TalonSRX frontRight = new WPI_TalonSRX(2);
+	
+	WPI_TalonSRX left2 = new WPI_TalonSRX(3);
+	WPI_TalonSRX right2 = new WPI_TalonSRX(4);
+	
+	DifferentialDrive drive = new DifferentialDrive(frontLeft, frontRight);
+	
+	Joystick joystick = new 
+	
+	
+	
+	*/
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
 	 */
 	@Override
 	public void robotInit() {
-		m_oi = new OI();
-		m_chooser.addDefault("Default Auto", new ExampleCommand());
-		// chooser.addObject("My Auto", new MyAutoCommand());
-		SmartDashboard.putData("Auto mode", m_chooser);
-	}
-
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
-	@Override
-	public void disabledInit() {
+		
+		oi = new OI();
+		//dt.calibrateGyro();
+		
+		SmartDashboard.putNumber("TurboSpeed", 0.95);
+		DriverStation.reportWarning("Robot Initiated", false);
 
 	}
-
-	@Override
-	public void disabledPeriodic() {
-		Scheduler.getInstance().run();
-	}
-
+	
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select
 	 * between different autonomous modes using the dashboard. The sendable
 	 * chooser code works with the Java SmartDashboard. If you prefer the
 	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
+	 * getString line to get the auto name from the text box below the Gyro
 	 *
-	 * <p>You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
+	 * <p>You can add additional auto modes by adding additional comparisons to
+	 * the switch structure below with additional strings. If using the
+	 * SendableChooser make sure to add them to the chooser code above as well.
 	 */
 	@Override
 	public void autonomousInit() {
-		m_autonomousCommand = m_chooser.getSelected();
 
-		/*
-		 * String autoSelected = SmartDashboard.getString("Auto Selector",
-		 * "Default"); switch(autoSelected) { case "My Auto": autonomousCommand
-		 * = new MyAutoCommand(); break; case "Default Auto": default:
-		 * autonomousCommand = new ExampleCommand(); break; }
-		 */
-
-		// schedule the autonomous command (example)
-		if (m_autonomousCommand != null) {
-			m_autonomousCommand.start();
-		}
 	}
 
 	/**
@@ -90,18 +85,8 @@ public class Robot extends TimedRobot {
 	 */
 	@Override
 	public void autonomousPeriodic() {
-		Scheduler.getInstance().run();
-	}
 
-	@Override
-	public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
-		if (m_autonomousCommand != null) {
-			m_autonomousCommand.cancel();
-		}
+	
 	}
 
 	/**
@@ -109,7 +94,12 @@ public class Robot extends TimedRobot {
 	 */
 	@Override
 	public void teleopPeriodic() {
-		Scheduler.getInstance().run();
+		
+		//DriverStation.reportWarning("Joystick X: " + oi.getXAxisOfArcade(), false);
+		//DriverStation.reportWarning("Joystick Y: " + oi.getYAxisOfArcade(), false);
+		
+		Robot.dt.driveLR(0, 0);
+		
 	}
 
 	/**
---
+++ Dragon 2/src/org/usfirst/frc/team6201/robot/subsystems/DriveTrain.java
@@ -0,0 +1,139 @@
+package org.usfirst.frc.team6201.robot.subsystems;
+
+import org.usfirst.frc.team6201.robot.Robot;
+import org.usfirst.frc.team6201.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ *
+ */
+public class DriveTrain extends Subsystem {
+
+	public DifferentialDrive drive;
+	
+	private WPI_TalonSRX backLeft = new WPI_TalonSRX(3);
+    private WPI_TalonSRX frontLeft = new WPI_TalonSRX(4);
+    SpeedControllerGroup leftDrive = new SpeedControllerGroup(frontLeft, backLeft);
+    
+    private WPI_TalonSRX frontRight = new WPI_TalonSRX(1);
+    private WPI_TalonSRX backRight = new WPI_TalonSRX(2);
+    SpeedControllerGroup rightDrive = new SpeedControllerGroup(frontRight, backRight);
+    
+    private Joystick joystick;
+    
+    private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+    
+    private double turnSens = 0.10;
+    
+    private double tanPower;
+    private double tanTurn;
+    
+    private double processedPower;
+    private double processedTurn;
+    
+    private final double TANDOMAIN_Y = 1.3;
+    private final double TANDOMAIN_X = 1.3;
+    
+    private double scaledValTan(double rawVal, double domain) {
+    	
+    	return Math.tan((rawVal * domain) / (Math.tan(domain)));
+    	
+    }
+    
+    public DriveTrain() {
+    	
+    	backLeft = new WPI_TalonSRX(RobotMap.LEFT_DRIVE1);
+    	frontLeft = new WPI_TalonSRX(RobotMap.LEFT_DRIVE2);
+    	frontRight = new WPI_TalonSRX(RobotMap.RIGHT_DRIVE1);
+    	backRight = new WPI_TalonSRX(RobotMap.RIGHT_DRIVE2); 
+    	    	
+    	frontLeft.setNeutralMode(NeutralMode.Brake);
+    	backLeft.setNeutralMode(NeutralMode.Brake);
+    	frontRight.setNeutralMode(NeutralMode.Brake);
+    	backRight.setNeutralMode(NeutralMode.Brake);
+    	
+    	drive = new DifferentialDrive(leftDrive, rightDrive);
+    	joystick = new Joystick(1);
+    	
+    }
+	
+    public void driveLR(double leftPower, double rightPower) {
+    	
+    	double joystickForward = joystick.getY();
+    	double joystickTurn = 1.0 * joystick.getY();
+    	double joystickSlider = 0.5 * (1 + (-1 * Robot.oi.getSliderAxisOfArcade()));
+/**    	
+    	tanPower = scaledValTan(joystickForward * joystickSlider, TANDOMAIN_X);
+    	tanTurn = scaledValTan(joystickTurn * joystickSlider, TANDOMAIN_Y);
+    	
+    	processedPower = tanPower;
+    	processedTurn = (1 - Math.abs(processedPower)) * tanTurn;
+**/    	
+    	
+    	if(Math.abs(joystickForward) < turnSens) {
+    		
+    		joystickForward = 0;
+    		
+    	}
+    	
+    	if(Math.abs(joystickTurn) < turnSens) {
+    		
+    		joystickTurn = 0;
+    		
+    	}    	
+
+    	
+//    	joystickForward = -(processedPower - processedTurn);
+//   	joystickTurn = -(processedPower + processedTurn);
+
+    	joystickForward = joystickForward * joystickSlider;
+    	joystickTurn = joystickTurn * joystickSlider;
+    	
+    	DriverStation.reportWarning("Left Power: " + joystickForward, false);
+    	DriverStation.reportWarning("Right Power: " + joystickTurn, false);
+    	
+    	drive.arcadeDrive(joystickForward, joystickTurn);
+    	
+    }
+    
+    public void initDefaultCommand() {
+
+    	
+    
+    }
+    
+    public void stop() {
+    	
+    	this.driveLR(0, 0);
+    	
+    }
+    
+    public void calibrateGyro() {
+    	
+    	gyro.calibrate();
+    	
+    }
+    
+    public void resetGyro() {
+    	
+    	gyro.reset();
+    	
+    }
+    
+    public void getGyroRate() {
+    	
+    	gyro.getRate();
+    	
+    }
+    
+    
+}
