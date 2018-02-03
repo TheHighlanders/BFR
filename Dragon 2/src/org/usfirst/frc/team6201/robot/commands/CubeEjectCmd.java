7bd54ed31c228e77eeae99e4b9c683f540fd1c4c
Baxter Ellard <baxter.ellard@gmail.com>
2018-02-03 16:50:51 -0500

    Cube Eject
    wrote command for cube ejection

---
+++ Dragon 2/src/org/usfirst/frc/team6201/robot/commands/CubeEjectCmd.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team6201.robot.commands;
+
+import org.usfirst.frc.team6201.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class CubeEjectCmd extends Command {
+
+    public CubeEjectCmd() {
+       
+    	requires(Robot.gi);
+    	
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	
+    	Robot.gi.eject();
+    	
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	
+    	Robot.gi.stop();
+    	
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	
+    	Robot.gi.stop();
+    	
+    }
+}
