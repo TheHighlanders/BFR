d534fc2d5272993ba41a5cfaf2de76cb1ba13592
Baxter Ellard <baxter.ellard@gmail.com>
2018-02-03 15:31:24 -0500

    Documentation
    Added documentation so we actually know what's goin' on.

--- Dragon 2/src/org/usfirst/frc/team6201/robot/OI.java
+++ Dragon 2/src/org/usfirst/frc/team6201/robot/OI.java
@@ -15,25 +15,48 @@ import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
- * This class is the glue that binds the controls on the physical operator
- * interface to the commands and command groups that allow control of the robot.
+ * This provides the framework to connect the DriverStation to the Robot both for getting values from the joystick(s), 
+ * and for starting commands when buttons are pressed.
+ * 
+ * 
+ * @author Baxter Ellard
+ * @author David Matthews
  */
 public class OI {
 	
+	/**
+	 * Create an object out of our logitech arcade joystick.
+	 * This allows us to get the  current position of the joystick, and the state of all the buttons.
+	 * Initialized with the USB devices plugged into the robot
+	 */
 	private Joystick logitech = new Joystick(RobotMap.LOGITECH);
 	
+	private Button b1 = new JoystickButton(logitech, 1);
+	
+	/**
+	 * @return  a double corresponding to how much the joystick's handle is rotated.
+	 * This has a range of -1 to 1. All the way to the right is +1.
+	 */
 	public double getRotationAxisOfArcade() {
 		
 		return logitech.getRawAxis(RobotMap.LOGITECH_ROTATE_AXIS);
 		
 	}
 	
+	/**
+	 * @return a double corresponding to the position of the joystick in the side to side direction (X axis).
+	 * Range of -1 to 1. All the way to the right is +1.
+	 */
 	public double getXAxisOfArcade() {
 		
 		return logitech.getRawAxis(RobotMap.LOGITECH_X_AXIS);
 		
 	}
 	
+	/**
+	 * @return a double corresponding to the position of the joystick in the Y axis (front and back).
+	 * range of -1 to 1, with all the way forward being 1
+	 */
 	public double getYAxisOfArcade() {
 		
 		return logitech.getRawAxis(RobotMap.LOGITECH_Y_AXIS);
@@ -51,8 +74,9 @@ public class OI {
 		
 	}
 	
-	private Button b1 = new JoystickButton(logitech, 1);
-	
+	/**
+	 * @return True if button 1 is pressed, false otherwise.
+	 */
 	public boolean getButton1() {
 		
 		return b1.get();
@@ -61,6 +85,7 @@ public class OI {
 	
 	public OI() {
 		
+		// starts the process of ascending to maximum elevator height
 		Button b9 = new JoystickButton(logitech, 9);
 		b9.toggleWhenPressed(new ElevatorAscendCmd());
 		
--- Dragon 2/src/org/usfirst/frc/team6201/robot/subsystems/DriveTrain.java
+++ Dragon 2/src/org/usfirst/frc/team6201/robot/subsystems/DriveTrain.java
@@ -7,26 +7,39 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- *
- */
+* The interface between the robot code and the actuators and sensors involved
+* in moving the robot. Right now this is just the motors and gyro, but this
+* will probably grow to include encoders.
+* 
+* TODO: Add a function that slows down the robot based on how high the elevator
+* is.
+* 
+* @author Baxter Ellard
+* @author David Matthews
+*/
+
 public class DriveTrain extends Subsystem {
 
+	// Instantiating TalonSRX motor controllers at CAN ports
+	// 1 through 4.
 	private WPI_TalonSRX left1 = new WPI_TalonSRX(1);
 	private WPI_TalonSRX left2 = new WPI_TalonSRX(2);
 	private WPI_TalonSRX right1 = new WPI_TalonSRX(3);
 	private WPI_TalonSRX right2 = new WPI_TalonSRX(4);
 		
+	// Gyro sensor
 	private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
 	
 	public static int forwardOrReverse = -1;
 	
+	/**
+	 * Constructor, sets up motors, prevents brownouts, and minimizes pedestrian
+	 * casualties.
+	 */
     public DriveTrain() {
-    	
     	
     	left1 = new WPI_TalonSRX(RobotMap.LEFT_DRIVE1);
     	left2 = new WPI_TalonSRX(RobotMap.LEFT_DRIVE2);
@@ -45,12 +58,24 @@ public class DriveTrain extends Subsystem {
     
     }
     
+    /**
+	 * ArcadeDriveCmd will always run when other commands are not busy. This
+	 * will allow operator control when the robot is not driving itself around.
+	 */
     public void initDefaultCommand() {
     	
     	setDefaultCommand(new ArcadeDriveCmd());
     	
     }
     
+    /**
+	 * Updates the motors with what speed to drive at.
+	 * 
+	 * @param leftPower
+	 *            Double speed of left motors. Range -1 to 1
+	 * @param rightPower
+	 *            Double speed of right motors. Range -1 to 1
+	 */
     public void driveLR(double leftPower, double rightPower) {
     	
   //  	DriverStation.reportWarning("Left Power: " + leftPower, false);
@@ -75,27 +100,53 @@ public class DriveTrain extends Subsystem {
     	
     }
     
+    /**
+	 * Sets the motors to be off.
+	 */
     public void stop() {
     	
     	this.driveLR(0, 0);
     	
     }
     
+    /**
+	 * Calibrates gyro (takes 5 seconds while robot does nothing) Do this when
+	 * robot first turns on.
+	 */
     public void calibrateGyro() {
     	
     	gyro.calibrate();
     	
     }
     
+    /**
+	 * Sets the drivetrain gyro back to 0 degrees
+	 */
     public void resetGyro() {
     	
     	gyro.reset();
     	
     }
     
+    /**
+	 * @return the current rate of turning in degrees per second
+	 */
     public void getGyroRate() {
     	
     	gyro.getRate();
+    	
+    }
+    
+	/**
+	 * 
+	 * @return gets an approximation of the gyro angle since reset was last
+	 *         called from an accumulation using the FPGA. Will accumulate error
+	 *         over time.
+	 * 
+	 */
+    public void getGyroAngle() {
+    	
+    	gyro.getAngle();
     	
     }
     
