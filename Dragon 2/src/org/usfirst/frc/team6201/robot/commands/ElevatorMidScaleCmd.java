5abcd6311fa621189cbe7f53781afebe27b0b961
Baxter Ellard <baxter.ellard@gmail.com>
2018-02-13 16:18:38 -0500

    COMANNDS
    spelling is cool
    we wrote some shit

--- Dragon 2/src/org/usfirst/frc/team6201/robot/commands/ElevatorMidScaleCmd.java
+++ Dragon 2/src/org/usfirst/frc/team6201/robot/commands/ElevatorMidScaleCmd.java
@@ -1,36 +1,51 @@
 package org.usfirst.frc.team6201.robot.commands;
 
+import org.usfirst.frc.team6201.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
  *
  */
 public class ElevatorMidScaleCmd extends Command {
+  
+	public ElevatorMidScaleCmd() {
 
-    public ElevatorMidScaleCmd() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+    	requires(Robot.el);
+    	
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	
+    	Robot.el.encoder.reset();
+    	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	
+    	Robot.el.ascend();
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return Robot.el.getEncoderRevs() >= 6;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	
+    	Robot.el.stop();
+    	
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	
+    	end();
+    	
     }
 }
